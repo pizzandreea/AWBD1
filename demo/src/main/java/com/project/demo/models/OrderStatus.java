24d24d6c273e1e07841db0cd813aadae3480d0e0
Mara-Luciana Belu <71257603+maraluciana@users.noreply.github.com>
2024-04-25 16:05:31 +0300

    added models : order, review, soup

---
+++ demo/src/main/java/com/project/demo/models/OrderStatus.java
@@ -0,0 +1,7 @@
+package com.project.demo.models;
+
+public enum OrderStatus {
+    STARTED,
+    COMPLETED,
+    SHIPPED
+}
---
+++ demo/src/main/java/com/project/demo/models/SoupType.java
@@ -0,0 +1,8 @@
+package com.project.demo.models;
+public enum SoupType {
+    VEGETARIAN,
+    CHICKEN,
+    BEEF,
+    SEAFOOD,
+    OTHER
+}
