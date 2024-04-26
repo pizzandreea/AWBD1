edf334a7fcdfee0ebbcbb61c3f5d6f93899cd9ba
pizzandreea <ciurescuandreea624@gmail.com>
2024-04-26 23:38:59 +0300

    soups list page displays soups from database http://localhost:8080/getAllSoups

--- demo/src/main/java/com/project/demo/controllers/SoupController.java
+++ demo/src/main/java/com/project/demo/controllers/SoupController.java
@@ -1,21 +1,17 @@
 package com.project.demo.controllers;
 
-import com.project.demo.dtos.SoupCreateDto;
 import com.project.demo.models.Soup;
-import com.project.demo.models.SoupType;
+
 import com.project.demo.services.SoupService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
-import java.net.URI;
-import java.net.URISyntaxException;
 import java.util.ArrayList;
 import java.util.List;
 
-@RestController
+@Controller
 @CrossOrigin
 public class SoupController {
     private final SoupService soupService;
@@ -27,8 +23,7 @@ public class SoupController {
 
     @GetMapping("/getAllSoups")
     public String getAll(Model model) {
-        List<Soup> soupsList = new ArrayList<Soup>();
-        soupsList = soupService.getAll();
+        List<Soup> soupsList = soupService.getAll();
         model.addAttribute("soups", soupsList);
         return "soups-list";
     }
